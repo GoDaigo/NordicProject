a434f6c35d1d95d9123280efc4a549f0ad30cd8a
GoDaigo <37551579+GoDaigo@users.noreply.github.com>
2018-03-19 16:19:26 -0400

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,21 @@
+import javax.swing.JFrame;
 
 public class Client {
+private JFrame j = new JFrame("Nordic Race");
 
-	public Client() {
-		
+
+public JFrame getFrame(){
+return j;	
+}
+
+
+
+	public Client(int width, int height) {
+		j.setSize(width, height);
+		j.setLocationRelativeTo(null);
+		j.setDefaultCloseOperation(3);
+		j.show();
+	
 	}
 	
 }
